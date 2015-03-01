c823df08c034299eac43bdb3f4c9c7fee19b0286
Stephen Cavaliere <stephencavaliere@gmail.com>
2015-02-28 22:54:59 -0500

    Initial Commit

---
+++ Gonzales/Gonzales.h
@@ -0,0 +1,19 @@
+//
+//  Gonzales.h
+//  Gonzales
+//
+//  Created by Stephen Cavaliere on 2/28/15.
+//  Copyright (c) 2015 Stephen Cavaliere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gonzales.
+FOUNDATION_EXPORT double GonzalesVersionNumber;
+
+//! Project version string for Gonzales.
+FOUNDATION_EXPORT const unsigned char GonzalesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gonzales/PublicHeader.h>
+
+
