00f30c71c7edbc6ca756df3ceb95c5f4acbb9233
Stephen Cavaliere <stephencavaliere@gmail.com>
2015-03-19 10:08:20 -0400

    Removed AssertTrue

--- GonzalesTests/Globals/ShouldTests.swift
+++ GonzalesTests/Globals/ShouldTests.swift
@@ -5,6 +5,6 @@ class ShouldTests: XCTestCase {
   func test_global_property_should_of_type_OptionalMatcher() {
     var matcher = should as Any
 
-    XCTAssertTrue(matcher is OptionalMatcher)
+    (matcher is OptionalMatcher).should.equal(true)
   }
 }
